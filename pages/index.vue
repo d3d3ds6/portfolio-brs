64a753d7ac925e7df60b763ca5996407ea1485cc
Brinis Abderahmen <brinisabderahmen@gmail.com>
2025-04-09 09:17:41 +0100

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -262,7 +262,7 @@
         <a
           target="_blank"
           class="flex h-14 w-48 flex-row items-center justify-center bg-gray-950 text-2xl font-bold uppercase tracking-widest"
-          href="https://drive.google.com/file/d/1U4QiTTF_2Rw3exkWC2gk2Dk7XaPFLE7A/view?usp=drive_link
+          href="https://drive.google.com/file/d/1gGv5Atg6OVo7ofZ9H6uF0liadWM0JGFf/view?usp=sharing
 "
           ><img
             class="mr-4 w-8"
